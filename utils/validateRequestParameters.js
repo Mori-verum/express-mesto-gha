18506b03310c941b53d2f70c3aa5eb1497b42b57
Selivokhina Anastasia <nastya.selivokhina8@gmail.com>
2023-04-17 15:34:31 +0300

    ref: add joi validation

--- utils/validateRequestParameters.js
+++ utils/validateRequestParameters.js
@@ -4,28 +4,37 @@ const { regex } = require('./validateUrl');
 
 const validateUserBody = celebrate({
   body: Joi.object().keys({
-    name: Joi.string().min(2).max(30)
+    name: Joi.string()
+      .min(2)
+      .max(30)
       .messages({
         'string.min': 'Минимальная длина поля "name" - 2',
         'string.max': 'Максимальная длина поля "name" - 30',
         'string.empty': 'Поле "name" должно быть заполнено',
       }),
-    about: Joi.string().min(2).max(30)
+    about: Joi.string()
+      .min(2)
+      .max(30)
       .messages({
         'string.min': 'Минимальная длина поля "about" - 2',
         'string.max': 'Максимальная длина поля "about" - 30',
       }),
-    avatar: Joi.string().custom(validateUrl)
+    avatar: Joi.string()
+      .custom(validateUrl)
       .message('Поле "avatar" должно быть валидным url-адресом')
+      .required()
       .messages({
         'string.empty': 'Поле "avatar" должно быть заполнено',
       }),
-    email: Joi.string().email().required()
+    email: Joi.string()
+      .email()
+      .message('Поле "email" должно быть валидным email-адресом')
+      .required()
       .messages({
-        'string.email': 'Поле "email" должно быть валидным email-адресом',
         'string.empty': 'Поле "email" должно быть заполнено',
       }),
-    password: Joi.string().required()
+    password: Joi.string()
+      .required()
       .messages({
         'string.empty': 'Поле "password" должно быть заполнено',
       }),
@@ -34,12 +43,15 @@ const validateUserBody = celebrate({
 
 const validateLoginParametrs = celebrate({
   body: Joi.object().keys({
-    email: Joi.string().required().email()
+    email: Joi.string()
+      .email()
+      .message('Поле "email" должно быть валидным email-адресом')
+      .required()
       .messages({
-        'string.email': 'Поле "email" должно быть валидным email-адресом',
         'string.empty': 'Поле "email" должно быть заполнено',
       }),
-    password: Joi.string().required()
+    password: Joi.string()
+      .required()
       .messages({
         'string.empty': 'Поле "password" должно быть заполнено',
       }),
@@ -48,13 +60,18 @@ const validateLoginParametrs = celebrate({
 
 const validateCardBody = celebrate({
   body: Joi.object().keys({
-    name: Joi.string().required().min(2).max(30)
+    name: Joi.string()
+      .required()
+      .min(2)
+      .max(30)
       .messages({
         'string.min': 'Минимальная длина поля "name" - 2',
         'string.max': 'Максимальная длина поля "name" - 30',
         'string.empty': 'Поле "name" должно быть заполнено',
       }),
-    link: Joi.string().required().pattern(regex)
+    link: Joi.string()
+      .required()
+      .pattern(regex)
       .message('Поле "link" должно быть валидным url-адресом')
       .messages({
         'string.empty': 'Поле "link" должно быть заполнено',
@@ -64,10 +81,13 @@ const validateCardBody = celebrate({
 
 const validateCardId = celebrate({
   params: Joi.object().keys({
-    cardId: Joi.string().length(24).hex().required()
+    cardId: Joi.string()
+      .length(24)
+      .hex()
+      .message('id должен быть в hex формате')
+      .required()
       .messages({
         'string.length': 'Длина id должна быть 24 символа',
-        'string.hex': 'id должен быть в hex формате',
         'string.empty': 'Поле "link" должно быть заполнено',
       }),
   }),
@@ -75,22 +95,29 @@ const validateCardId = celebrate({
 
 const validateUserId = celebrate({
   params: Joi.object().keys({
-    userId: Joi.string().length(24).hex().required()
+    userId: Joi.string()
+      .length(24)
+      .hex()
+      .message('id должен быть в hex формате')
+      .required()
       .messages({
         'string.length': 'Длина id должна быть 24 символа',
-        'string.hex': 'id должен быть в hex формате',
       }),
   }),
 });
 
 const validateUserInfo = celebrate({
   body: Joi.object().keys({
-    name: Joi.string().min(2).max(30)
+    name: Joi.string()
+      .min(2)
+      .max(30)
       .messages({
         'string.min': 'Минимальная длина поля "name" - 2',
         'string.max': 'Максимальная длина поля "name" - 30',
       }),
-    about: Joi.string().min(2).max(30)
+    about: Joi.string()
+      .min(2)
+      .max(30)
       .messages({
         'string.min': 'Минимальная длина поля "about" - 2',
         'string.max': 'Максимальная длина поля "about" - 30',
@@ -100,8 +127,11 @@ const validateUserInfo = celebrate({
 
 const validateUserAvatar = celebrate({
   body: Joi.object().keys({
-    avatar: Joi.string().min(2).pattern(regex).required()
+    avatar: Joi.string()
+      .min(2)
+      .pattern(regex)
       .message('Поле "avatar" должно быть валидным url-адресом')
+      .required()
       .messages({
         'string.min': 'Минимальная длина поля "avatar" - 2',
         'string.empty': 'Поле "avatar" должно быть заполнено',
